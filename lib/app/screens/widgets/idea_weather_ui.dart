1106fd15f04804f0fe8a77e6e416eb0438528efa
Neal <priboi78046@gmail.com>
2024-02-18 04:41:36 +0530

    Added - Weather code and max min temp for the current day

--- lib/app/screens/widgets/idea_weather_ui.dart
+++ lib/app/screens/widgets/idea_weather_ui.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_weather_2/core/providers/weather_provider.dart';
 import 'package:gap/gap.dart';
 import 'package:glass_kit/glass_kit.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WeatherUi extends ConsumerWidget {
   const WeatherUi({super.key});
@@ -36,26 +37,86 @@ class WeatherUi extends ConsumerWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const Text(
-                          'Location',
-                          style: TextStyle(
-                            fontSize: 22,
-                            letterSpacing: 0.6,
-                            fontWeight: FontWeight.w200,
-                          ),
-                        ),
-                        const Gap(40),
                         Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Text(
-                              '${data!.current.temperature_2m}${data.current_units.temperature_2m}',
-                              style: const TextStyle(
-                                fontSize: 60,
-                                fontWeight: FontWeight.w600,
-                                letterSpacing: 1.5,
+                            const Row(
+                              children: [
+                                Icon(Icons.pin_drop_rounded),
+                                Gap(4),
+                                Text(
+                                  'Location',
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    letterSpacing: 0.6,
+                                    fontWeight: FontWeight.w200,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.search_rounded,
                               ),
                             ),
                           ],
+                        ),
+                        const Gap(40),
+                        Text(
+                          '${data!.current.temperature_2m}${data.current_units.temperature_2m}',
+                          style: GoogleFonts.bebasNeue(
+                            fontSize: 85,
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 1,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            right: 8,
+                            top: 20,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              const Text(
+                                'Cloudy',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 46,
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    '${data.daily.temperature_2m_min[0]}${data.daily_units.temperature_2m_min}',
+                                    style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w300,
+                                      color: Colors.grey.shade500,
+                                    ),
+                                  ),
+                                  Text(
+                                    ' - ',
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w300,
+                                      color: Colors.grey.shade500,
+                                    ),
+                                  ),
+                                  Text(
+                                    '${data.daily.temperature_2m_max[0]}${data.daily_units.temperature_2m_max}',
+                                    style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w300,
+                                      color: Colors.grey.shade500,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                         Padding(
                           padding: const EdgeInsets.only(right: 26, top: 40),
